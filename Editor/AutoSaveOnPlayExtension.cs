a2b65a89d56d4bfc9f91ad7d8e09daad43a5ea2b
John Dabrowski <formatc2013@gmail.com>
2020-04-06 08:20:05 +0100

    Initial commit

---
+++ Editor/AutoSaveOnPlayExtension.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+[InitializeOnLoad]
+public class AutoSaveOnPlayExtension
+{
+    // Static constructor that gets called when unity fires up.
+    static AutoSaveOnPlayExtension()
+    {
+        EditorApplication.playmodeStateChanged += AutoSaveWhenPlaymodeStarts;
+    }
+
+    private static void AutoSaveWhenPlaymodeStarts()
+    {
+        // If we're about to run the scene...
+        if (EditorApplication.isPlayingOrWillChangePlaymode && !EditorApplication.isPlaying)
+        {
+            AutoSave();
+        }
+    }
+
+    private static void AutoSave()
+    {
+        EditorSceneManager.SaveScene(EditorSceneManager.GetActiveScene());
+        AssetDatabase.SaveAssets();
+        Debug.Log("Autosave carried out @: " + System.DateTime.Now);
+    }
+}
